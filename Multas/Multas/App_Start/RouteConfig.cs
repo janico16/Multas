70a87073830ff7b2350e5f517ddd00767009faff
Tiago Duarte <aluno20474@ipt.pt>
2019-04-09 16:24:40 +0100

    Adição do controller AGENTES + bootstrap e jQuerry para formatação das Views

--- Multas/Multas/App_Start/RouteConfig.cs
+++ Multas/Multas/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Multas
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AgentesController", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
